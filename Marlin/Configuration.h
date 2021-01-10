96c285c9bfcc2386e0e981be6ac956f0a3301fcd
Marcello Serra <marcello.serra962@gmail.com>
2021-01-10 17:24:36 +0100

    adjust printbed area 230 210

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1129,8 +1129,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 300
-#define Y_BED_SIZE 300
+#define X_BED_SIZE 230
+#define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
